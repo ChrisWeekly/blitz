b90bd3d3b88698b642348cebfd9c58b4b9a52f91
Rudi Yardley <contact@rudiyardley.com>
2020-04-21 17:15:16 +1000

    --wip-- [skip ci]

--- packages/server/src/synchronizer/pipeline/rules/config/index.ts
+++ packages/server/src/synchronizer/pipeline/rules/config/index.ts
@@ -5,14 +5,14 @@ import File from 'vinyl'
 import {through} from '../../../streams'
 import {Rule} from '../../../types'
 
-// const isBlitzConfigPath = (p: string) => /blitz\.config\.(js|ts)/.test(p)
+const isNextConfigPath = (p: string) => /next\.config\.(js|ts)/.test(p)
 
 const create: Rule = ({config, input}) => {
   // Preconditions
   const hasNextConfig = pathExistsSync(resolve(config.src, 'next.config.js'))
   const hasBlitzConfig = pathExistsSync(resolve(config.src, 'blitz.config.js'))
 
-  if (hasNextConfig) {
+  if (hasNextConfig && !process.env.NOW_BUILDER) {
     // TODO: Pause the stream and ask the user if they wish to have their configuration file renamed
     const err = new Error(
       'Blitz does not support next.config.js. Please rename your next.config.js to blitz.config.js',
@@ -32,22 +32,47 @@ const create: Rule = ({config, input}) => {
     )
   }
 
-  const nextConfigShellTpl = `
+  if (!hasNextConfig) {
+    input.write(
+      new File({
+        cwd: config.src,
+        path: resolve(config.src, 'next.config.js'),
+        contents: Buffer.from(`
 const {withBlitz} = require('@blitzjs/server');
 const config = require('./blitz.config.js');
 module.exports = withBlitz(config);
-`
-
-  input.write(
-    new File({
-      cwd: config.src,
-      path: resolve(config.src, 'next.config.js'),
-      contents: Buffer.from(nextConfigShellTpl),
-    }),
-  )
+        `),
+      }),
+    )
+  }
 
   // No need to filter yet
-  const stream = through({objectMode: true}, (file: File, _, next) => next(null, file))
+  const stream = through({objectMode: true}, (file: File, _, next) => {
+    if (!isNextConfigPath(file.path)) return next(null, file)
+    // Zeit now adds configuration needed for Now, like serverless target,
+    // so we need to keep and use that
+    if (process.env.NOW_BUILDER) {
+      // Assume we have a next.config.js if NOW_BUILDER is true as the cli creates one
+
+      // Divert next.config to next-zeit.config.js
+      input.write(
+        new File({
+          cwd: config.src,
+          path: resolve(config.src, 'next-zeit.config.js'),
+          contents: file.contents,
+        }),
+      )
+
+      file.contents = Buffer.from(`
+const {withBlitz} = require('@blitzjs/server');
+const zeitConfig = require('./next-zeit.config.js');
+const config = require('./blitz.config.js');
+module.exports = withBlitz({...config, ...zeitConfig});
+      `)
+    }
+
+    next(null, file)
+  })
 
   return {stream}
 }
